adfaac9e78a2d1203878a004fa6ada2ff30dac82
Jongho Bae <jb4918@columbia.edu>
2024-08-19 11:29:51 -0400

    getthought, createthought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,35 @@
 const { Thought, User } = require("../models");
 
-module.exports = {};
+module.exports = {
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async createThought(req, res) {
+    try {
+      const thoughtData = await Thought.create(req.body);
+      res.json(thoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getThought(req, res) {
+    try {
+      const thought = await Thought.findOne({
+        _id: req.params.thoughtId,
+      });
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 
-const {} = require("../../controllers/thoughtController");
+const {
+  getThoughts,
+  createThought,
+  getThought,
+} = require("../../controllers/thoughtController");
 // /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getThought);
 
 // /api/thoughts/:thoughtId/reactions
+
+module.exports = router;
