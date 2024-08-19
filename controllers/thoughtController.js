4e4709990870835fdf1dcc05671951911c4c34f9
Jongho Bae <jb4918@columbia.edu>
2024-08-19 12:23:58 -0400

    Added thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -12,7 +12,17 @@ module.exports = {
   async createThought(req, res) {
     try {
       const thoughtData = await Thought.create(req.body);
-      res.json(thoughtData);
+
+      const thoughtOwner = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: thoughtData._id } },
+        { new: true }
+      );
+      if (!thoughtOwner) {
+        return res.status(404).json({ message: "No user found with that id" });
+      }
+
+      res.json(thoughtOwner);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -30,6 +40,74 @@ module.exports = {
       res.json(thought);
     } catch (err) {
       res.status(500).json(err);
+    }
+  },
+
+  async updateThought(req, res) {
+    try {
+      const updatedThought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body }
+      );
+
+      if (!updatedThought) {
+        return res.status(404).json({ message: "No thought with this ID" });
+      }
+
+      res.json(updatedThought);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  },
+
+  async deleteThought(req, res) {
+    try {
+      const deletedThought = await Thought.findOneAndDelete({
+        _id: req.params.thoughtId,
+      });
+
+      if (!deletedThought) {
+        return res.status(404).json({ message: "No thought with this ID" });
+      }
+
+      res.json(deletedThought);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  },
+
+  async createReaction(req, res) {
+    try {
+      const reaction = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } }
+      );
+
+      if (!reaction) {
+        return res.status(404).json({ message: "No thought with this ID" });
+      }
+
+      res.json(reaction);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  },
+  async deleteReaction(req, res) {
+    try {
+      const reaction = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { _id: req.body.reactionId } } }
+      );
+
+      if (!reaction) {
+        return res
+          .status(404)
+          .json({ message: "No thought or reaction with this ID" });
+      }
+
+      res.json(reaction);
+    } catch (e) {
+      res.status(500).json(e);
     }
   },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,13 +4,25 @@ const {
   getThoughts,
   createThought,
   getThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtController");
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getThought);
+router
+  .route("/:thoughtId")
+  .get(getThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions")
+  .post(createReaction)
+  .delete(deleteReaction);
 
 module.exports = router;
