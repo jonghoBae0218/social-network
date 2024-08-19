914cbee0ba872c0d42e7a543fa1dd8c7967dd02d
Jongho Bae <jb4918@columbia.edu>
2024-08-18 22:22:13 -0400

    Added deleteuser updateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,4 +33,34 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user found with that id" });
+      }
+      return res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({
+        _id: req.params.id,
+      });
+      if (!user) {
+        return res.status(404).json({ message: "No user found with that id" });
+      }
+      return res.json("Deleted sucessfully");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,15 @@ const {
   getUsers,
   createUser,
   getUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:id
-router.route("/:id").get(getUser);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 router.route("/:userId/friends/:friendId").get();
